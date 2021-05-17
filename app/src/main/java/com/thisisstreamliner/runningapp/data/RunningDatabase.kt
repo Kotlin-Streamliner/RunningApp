a0bb79341d214f71a5c2ce9f0306d085abd08d79
thisisstreamliner <thisisstreamliner@gmail.com>
2021-05-17 14:21:40 +0300

    Create RunningDatabase, Create TypeConverter for complex objects

---
+++ app/src/main/java/com/thisisstreamliner/runningapp/data/RunningDatabase.kt
@@ -0,0 +1,12 @@
+package com.thisisstreamliner.runningapp.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(entities = [Run::class], version = 1)
+@TypeConverters(TypeConverter::class)
+abstract class RunningDatabase : RoomDatabase() {
+
+    abstract fun getRunDao(): RunDAO
+}
---
+++ app/src/main/java/com/thisisstreamliner/runningapp/data/TypeConverter.kt
@@ -0,0 +1,20 @@
+package com.thisisstreamliner.runningapp.data
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.room.TypeConverter
+import java.io.ByteArrayOutputStream
+
+class TypeConverter {
+
+    @TypeConverter
+    fun fromBitmap(bitmap: Bitmap): ByteArray {
+        val outputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+        return outputStream.toByteArray()
+    }
+
+    @TypeConverter
+    fun toBitmap(byteArray: ByteArray): Bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+
+}
